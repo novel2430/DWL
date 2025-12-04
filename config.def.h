54dd18f0bfe3c626e3a54afb5d6e266f2fd12e56
novel2430 <novel2430@163.com>
2025-12-04 23:57:07 +0800

    update

--- config.def.h
+++ config.def.h
@@ -131,8 +131,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-// static const char *termcmd[] = { "wezterm", NULL };
-static const char *termcmd[] = { "foot", NULL };
+static const char *termcmd[] = { "wezterm", NULL };
 static const char *menucmd[] = { "rofi", "-show", "drun", NULL };
 static const char *runmenucmd[] = { "rofi", "-show", "run", NULL };
 static const char *browsercmd[] = { "zen-browser", NULL };
